913864a1316e8612e7ec37c9048b3e6a40f230df
Eric Rivera <65928359+C0deRunner15@users.noreply.github.com>
2021-03-15 22:43:06 -0400

    Update weekEnding3-13.js

--- weekEnding3-13.js
+++ weekEnding3-13.js
@@ -18,3 +18,24 @@ function hello(name) {
 
 //March 10: (8Kyu)Write a function 'detect' that will check if a string starts with 'Can someone explain', case sensitive. Return true or false.
 const detect = comment => comment.startsWith('Can someone explain');
+
+//March 11: (8Kyu)Find the Maximum and Minimum Values of a List
+const min = list => Math.min(...list);
+
+const max = list => Math.max(...list);
+
+//March 12: (6Kyu)Write a function that takes a string of one or more words, and returns the same string, but with all five or more letter words reversed. testCase = ('Hey fellow warriors') => returns 'Hey wollef sroirraw'
+function spinWords(str){
+  let wordsSplit = str.split(' ')
+  for(let i = 0; i < wordsSplit.length; i++){
+    let splitStr = wordsSplit[i];
+    if(splitStr.length >= 5){
+      splitStr = splitStr.split('').reverse().join('');
+      wordsSplit.splice(i, 1, splitStr); 
+    }
+  }
+  return wordsSplit.join(' ');
+}
+
+//March 13: (8Kyu)Write a function that takes a number, and returns the number of litres that a person will drink in that time, rounded down. For every hour, a person drinks 0.5 litres of water. 
+const litres = time => Math.floor(time * 0.5);
