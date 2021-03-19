954c760aebb3fba6ac67b3b0b6791e4deb5513ec
Eric Rivera <65928359+C0deRunner15@users.noreply.github.com>
2021-03-18 21:44:57 -0400

    Update weekEnding3-20.js

--- weekEnding3-20.js
+++ weekEnding3-20.js
@@ -2,3 +2,8 @@
 function boolToWord( bool ){
   return bool === true ? 'Yes':'No';
 }
+
+//3-18: (8Kyu) Write a function that determines whether number n is divisible by two other numbers, x and y.
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
