7c1c30bd5f0e5cdfec09fcec040677a081e56c3b
Eric Rivera <65928359+C0deRunner15@users.noreply.github.com>
2021-03-20 19:46:59 -0400

    Update weekEnding3-20.js

--- weekEnding3-20.js
+++ weekEnding3-20.js
@@ -7,3 +7,9 @@ function boolToWord( bool ){
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0
 }
+
+//3-20: (7Kyu) Write a function that takes a non-negative integer and returns an integer sorted in descending order.
+function descendingOrder(n){
+  let nArr = Array.from(n.toString()).map(Number);
+  return Number(nArr.sort((a,b) => b - a).join(''));//...
+}
